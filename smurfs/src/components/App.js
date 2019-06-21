15e03237274543bccd5795c54f7b5fd914c8c804
Blake Goms <blakegoms@gmail.com>
2019-06-21 12:37:25 -0700

    MVP:Answers.md

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,12 +3,7 @@ import { connect } from 'react-redux';
 import './App.css';
 import { getSmurfs, addSmurf } from '../actions'
 import SmurfList from './SmurfList';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+
 class App extends Component {
     state = {
       name: '',
@@ -74,4 +69,4 @@ const mapStateToProps = state => ({
   error: state.error
 })
 //conneting MSTP, bringing in action creators from getSmurfs, connecting it to the component addSmurf
-export default connect(mapStateToProps, { getSmurfs, addSmurf })(App);
+export default connect(mapStateToProps, { getSmurfs, addSmurf })(App); 
